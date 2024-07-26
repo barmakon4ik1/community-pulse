c97af9991830bf98ad1bab71fb7c76c65caea861
Viascheslav Bandylo <viacheslav.bandylo@yandex.ru>
2024-07-26 11:11:10 +0300

    Update route/questions

--- app/routes/questions.py
+++ app/routes/questions.py
@@ -1,16 +1,28 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify, make_response
+
+from app.models import Question, db
 
 questions_bp = Blueprint('questions', __name__, url_prefix='/questions')
 
 
 @questions_bp.route('/', methods=['GET'])
 def get_questions():
-    return '<h1>Questions</h1>'
+    questions = Question.query.all()
+    questions_data = [{'id': q.id, 'text': q.text} for q in questions]
+    return jsonify(questions_data)
 
 
 @questions_bp.route('/', methods=['POST'])
 def create_question():
-    return 'Question was created'
+    data = request.get_json()
+    if not data or 'text' not in data:
+        return jsonify({'error': 'Missing data'}), 400
+
+    question = Question(text=data['text'])
+    db.session.add(question)
+    db.session.commit()
+
+    return jsonify({'message': 'Question created', 'id': question.id}), 201
 
 
 @questions_bp.route('/<int:question_id>', methods=['GET'])
